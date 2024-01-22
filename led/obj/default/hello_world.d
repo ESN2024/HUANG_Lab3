d8bc04212d87985ed9461523575cec7755a79fc3
Hszcoco97 <94178929+Hszcoco97@users.noreply.github.com>
2024-01-22 13:02:33 +0100

    Add files via upload

---
+++ led/obj/default/hello_world.d
@@ -0,0 +1,24 @@
+obj/default/hello_world.o: hello_world.c ../led_bsp/system.h \
+ ../led_bsp/linker.h ../led_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../led_bsp//HAL/inc/io.h ../led_bsp//HAL/inc/alt_types.h \
+ ../led_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../led_bsp//HAL/inc/sys/alt_irq.h ../led_bsp//HAL/inc/nios2.h \
+ ../led_bsp//HAL/inc/alt_types.h
+
+../led_bsp/system.h:
+
+../led_bsp/linker.h:
+
+../led_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../led_bsp//HAL/inc/io.h:
+
+../led_bsp//HAL/inc/alt_types.h:
+
+../led_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../led_bsp//HAL/inc/sys/alt_irq.h:
+
+../led_bsp//HAL/inc/nios2.h:
+
+../led_bsp//HAL/inc/alt_types.h:
